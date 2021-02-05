bac36b13902e3c33b49f152ac9e583e51f2f4af9
magsa79 <magnus.gustafsson2@hm.com>
2021-02-05 13:30:17 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number 1: ");
+        int number1 = scanner.nextInt();
+        System.out.println("Enter number 2: ");
+        int number2 = scanner.nextInt();
+
+
+
+    }
+
+}
